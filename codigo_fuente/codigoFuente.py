ac07456e4f5d3693f6eefe65277ca123eba81a10
Esther Mariana Chunga Pacheco <echungap@ulasalle.edu.pe>
2025-03-25 21:39:47 -0500

    Editando codigo fuente

--- codigo_fuente/codigoFuente.py
+++ codigo_fuente/codigoFuente.py
@@ -1,35 +1,17 @@
 import ply.lex as lex
 
-# Lista de nombres de tokens
 tokens = (
-    # Strings primero para darles prioridad
     'DT_STRING',
-    # Variables, constantes y tipos de datos
     'VAR_KEYWORD', 'VAR', 'CONST', 'DT_INT', 'DT_FLOAT', 'DT_CHAR', 'DT_BOOL',
-    
-    # Estructuras de control
     'IF', 'ELSE', 'SWITCH', 'FOR', 'WHILE', 'DO_WHILE',
-    
-    # Control de flujo
     'RETURN', 'BREAK', 'CONTINUE',
-    
-    # Operadores de flujo
-    'COUT', 'CIN', 'O_IN', 'ENDLINE',
-    
-    # Operadores aritméticos
+    'COUT', 'CIN', 'O_IN', 'O_EX', 'ENDLINE',
     'O_SUMA', 'O_REST', 'O_MULT', 'O_DIV', 'O_MOD',
-    
-    # Operadores relacionales
-    'O_IGUALDAD', 'O_DIFERENTE', 'O_MAYOR', 'O_MENOR', 'O_MAYORIGUAL',
-    
-    # Símbolos especiales
+    'O_IGUALDAD', 'O_DIFERENTE', 'O_MAYOR', 'O_MENOR', 'O_MAYORIGUAL', 'O_ASIG',
     'SYM_PAR_IZQ', 'SYM_PAR_DER', 'SYM_LLAVE_IZQ', 'SYM_LLAVE_DER',
-    
-    # Varios
     'FUNCTION', 'E_INS', 'COM_SIMPLE', 'COM_MULTI'
 )
 
-# Expresiones regulares para tokens simples
 t_O_SUMA = r'\+'
 t_O_REST = r'-'
 t_O_MULT = r'\*'
@@ -40,18 +22,20 @@ t_O_DIFERENTE = r'!='
 t_O_MAYOR = r'>'
 t_O_MENOR = r'<'
 t_O_MAYORIGUAL = r'>='
+t_O_ASIG = r'='
 t_SYM_PAR_IZQ = r'\('
 t_SYM_PAR_DER = r'\)'
 t_SYM_LLAVE_IZQ = r'\{'
 t_SYM_LLAVE_DER = r'\}'
-t_O_IN = r'<<|>>'
-t_ENDLINE = r';|ENDLINE'
+t_O_IN = r'<<'
+t_O_EX = r'>>'
+t_ENDLINE = r'ENDLINE'
+t_E_INS = r';'
 
 def t_VAR_KEYWORD(t):
     r'VAR(?![a-zA-Z0-9-])'
     return t
 
-# Tokens más complejos
 def t_VAR(t):
     r'V-[a-zA-Z]+-[0-9]+'
     return t
@@ -76,12 +60,15 @@ def t_DT_BOOL(t):
     return t
 
 def t_DT_STRING(t):
-    r'"([^"]|\n)*"'
-    t.value = t.value  # Mantener las comillas
+    r'\".+\"'
     return t
 
 def t_IF(t):
     r'IF'
+    return t
+
+def t_FOR(t):
+    r'FOR'
     return t
 
 def t_RETURN(t):
@@ -105,10 +92,9 @@ def t_COM_SIMPLE(t):
     pass
 
 def t_COM_MULTI(t):
-    r'/\*(.|\n)*?\*/'
+    r'/\(.|\n)?\*/'
     pass
 
-# Caracteres ignorados
 t_ignore = ' \t'
 
 def t_newline(t):
@@ -119,7 +105,6 @@ def t_error(t):
     print(f"Carácter ilegal '{t.value[0]}'")
     t.lexer.skip(1)
 
-# Construir el lexer
 lexer = lex.lex()
 
 def analyze_file(filename):
@@ -136,7 +121,6 @@ def analyze_file(filename):
     except Exception as e:
         print(f"Error al leer el archivo: {str(e)}")
 
-# Test
 if __name__ == "__main__":
     filename = input("Ingrese el nombre del archivo a analizar: ")
     analyze_file(filename)
